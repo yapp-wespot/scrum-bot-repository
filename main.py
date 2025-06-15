9a8012f8d6c35e7fa857641b67558c5a53c84d70
kpeel <jaeyeon.kim@seavantage.com>
2025-06-15 16:47:35 +0900

    feat: 토요일만 가동될 수 있도록 설정

--- main.py
+++ main.py
@@ -11,58 +11,65 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
 
 messages = [
-    "고통의 월요일 ♥️\n"
-    + "데일리 스크럼 시간이에요 BFF ⏰\n"
-    + "주말에 한 일과 오늘 할 일을 공유해보아요 🔥\n"
-    + "레 츠 고 우 🚀",
-    # "절망의 화요일 ♥️\n"+
-    # "데일리 스크럼 시간이에요 BFF ⏰\n"+
-    # "어제 한 일과 오늘 할 일을 공유해보아요 🔥\n"+
-    # "레 츠 고 우 🚀",
-    "인내의 수요일 ♥️\n"
-    + "데일리 스크럼 시간이에요 BFF ⏰\n"
-    + "어제 한 일과 오늘 할 일을 공유해보아요 🔥\n"
-    + "오늘 하루도 화이팅 🚀",
-    # "희망의 목요일 ♥️\n"+
-    # "데일리 스크럼 시간이에요 BFF ⏰\n"+
-    # "어제 한 일과 오늘 할 일을 공유해보아요 🔥\n"+
-    # "레 츠 고 우 🚀",
-    "환희의 금요일 ♥️\n"
-    + "데일리 스크럼 시간이에요 BFF ⏰\n"
-    + "어제 한 일과 오늘 할 일을 공유해보아요 🔥\n"
-    + "어쩔 티비도 화이팅 🚀",
-    # "",
-    "⭐️행복한 위스팟의 2025년을 만들어주세요!⭐️"
-    + "벌써 돌아온 일요일?!😲"
-    + "이번주 한 일과 다음주에 할 일을 공유해보아요🔥"
-    + "가 보 자 고🔫",
+  """고통의 월요일 ♥️
+  데일리 스크럼 시간이에요 BFF ⏰
+  주말에 한 일과 오늘 할 일을 공유해보아요 🔥
+  레 츠 고 우 🚀""",
+  """절망의 화요일 ♥️
+  데일리 스크럼 시간이에요 BFF ⏰
+  어제 한 일과 오늘 할 일을 공유해보아요 🔥
+  레 츠 고 우 🚀""",
+  """인내의 수요일 ♥️
+  데일리 스크럼 시간이에요 BFF ⏰
+  어제 한 일과 오늘 할 일을 공유해보아요 🔥
+  오늘 하루도 화이팅 🚀""",
+  """희망의 목요일 ♥️
+  데일리 스크럼 시간이에요 BFF ⏰
+  어제 한 일과 오늘 할 일을 공유해보아요 🔥
+  레 츠 고 우 🚀""",
+  """환희의 금요일 ♥️
+  데일리 스크럼 시간이에요 BFF ⏰
+  어제 한 일과 오늘 할 일을 공유해보아요 🔥
+  어쩔 티비도 화이팅 🚀""",
+  """위클리 스크럼 시간이에요 BFF🏋🏻
+  시간이 너무 빠르다 빨라⏰
+  이번주 한 일과 다음주에 할 일을 공유해보아요 🚀
+  매주 토요일 마다 반복할 수 있을까용!
+  """,
+  """⭐️행복한 위스팟의 2025년을 만들어주세요!⭐️
+  벌써 돌아온 일요일?!😲
+  이번주 한 일과 다음주에 할 일을 공유해보아요🔥
+  가 보 자 고🔫"""
 ]
+
 
 # DATES=[datetime(2024, 7, 31, 23, 0), datetime(2024, 8, 21, 23, 0), datetime(2024, 8, 31, 23, 0), datetime(2024, 9, 14, 23, 0), datetime(2024, 9, 14, 23, 0)]
 # MESSAGES_BY_DATES=["📢 2차 스프린트 마감 D-{} 📢", "📢 이제는 더 이상 물러날 곳이 없다 D-{} 📢", "📢 데모데이 두 과 자 🥰 D-{} 📢","📢 1등 두 과 자 🥰 D-{} 📢", "📢 홍보 두 과 자 🥰 📢"]
 
-
 async def send_daily_message():
-    await client.wait_until_ready()
-    channel = client.get_channel(CHANNEL_ID)
-    now = datetime.now()
-    month = str(now.month)
-    day = str(now.day)
+  await client.wait_until_ready()
+  channel = client.get_channel(CHANNEL_ID)
+  now = datetime.now()
+  month = str(now.month)
+  day = str(now.day)
 
-    weekday = now.weekday()
-    kr_holidays = holidays.KR(years=now.year)
+  weekday = now.weekday()
+  # kr_holidays = holidays.KR(years=now.year)
 
-    if weekday != 6 and (now.date() in kr_holidays or weekday % 2 != 0):
-        return
+  if weekday != 5:  # This workflow run only on Saturdays
+    return
 
-    if channel:
-        # dday_message = getDdayMessage(now)
-        daily_message = f"{month}/{day} {messages[weekday//2]}"
-        # createMessage = await channel.send(dday_message+"\n\n"+daily_message)
-        createMessage = await channel.send(daily_message)
-        await createMessage.create_thread(name=month + "/" + day)
-    else:
-        print(f"Cannot find channel with ID {str(CHANNEL_ID)}")
+  # if weekday != 6 and (now.date() in kr_holidays or weekday % 2 != 0):
+  #   return
+
+  if channel:
+    # dday_message = getDdayMessage(now)
+    daily_message = f"{month}/{day} {messages[weekday]}"
+    # createMessage = await channel.send(dday_message+"\n\n"+daily_message)
+    created_message = await channel.send(daily_message)
+    await created_message.create_thread(name=month + "/" + day)
+  else:
+    print(f"Cannot find channel with ID {str(CHANNEL_ID)}")
 
 
 # def getDdayMessage(now):
@@ -85,11 +92,11 @@ async def send_daily_message():
 
 @client.event
 async def on_ready():
-    await client.change_presence(
-        status=discord.Status.online, activity=discord.Game("스크럼 봇 가동")
-    )
-    await send_daily_message()
-    await client.close()
+  await client.change_presence(
+      status=discord.Status.online, activity=discord.Game("스크럼 봇 가동")
+  )
+  await send_daily_message()
+  await client.close()
 
 
 client.run(TOKEN)
